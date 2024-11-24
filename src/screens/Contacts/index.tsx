ef027343409f9f3cd604e1b9ae74c7750c5e7268
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-11-24 00:20:09 -0300

    Criado componentes da tela de login e implementados, adicionado mapa da cidade de ilheus

--- src/screens/Contacts/index.tsx
+++ src/screens/Contacts/index.tsx
@@ -1,6 +1,6 @@
   import {ScrollView, Text, View, Linking, StyleSheet  } from "react-native";
   import { HeaderApp } from "../../components/Header"
-  import { Container, SubTitle, TitleSubContainer, Title, TextSubTitle, ContainerText, TitleInContainer, IconContainer, ContainerText2, TitleInContainer2} from "./style";
+  import { Container, SubTitle, TitleSubContainer, Title, TextSubTitle, ContainerText, TitleInContainer, IconContainer, ContainerText2, TitleInContainer3, TitleInContainer2} from "./style";
   import { useNavigation } from "@react-navigation/native";
   import { RootNavigationProp } from "src/types/navigation";
   import Ionicons from '@expo/vector-icons/Ionicons';
@@ -13,7 +13,6 @@
     
     return (
       <ScrollView>
-        <HeaderApp />
         <SubTitle>
         <TitleSubContainer>Início</TitleSubContainer>
         <TitleSubContainer>Formulários</TitleSubContainer>
@@ -22,7 +21,7 @@
         </SubTitle>
         <Title>SITES</Title>
         <TextSubTitle>Registro de Boletim de Ocorrência</TextSubTitle>
-        <ContainerText><TitleInContainer>https://delegaciavirtual.sinesp.gov.br/portal/</TitleInContainer></ContainerText>
+        <ContainerText><TitleInContainer3>https://delegaciavirtual.sinesp.gov.br/portal/</TitleInContainer3></ContainerText>
         <Title>CONTATOS</Title>
         <TextSubTitle>Emergência Policial</TextSubTitle>
         <ContainerText><TitleInContainer>190</TitleInContainer></ContainerText>
@@ -33,17 +32,41 @@
         <ContainerText2><IconContainer><Ionicons name="location-sharp" size={40} color="black" /></IconContainer><TitleInContainer2>Mapa - Unidades Policiais</TitleInContainer2></ContainerText2>
         <View style={styles.container}>
           <Map style={StyleSheet.absoluteFill}
-          
-          
+               initialRegion={{
+                latitude: -14.803964463346052,
+                longitude: -39.0316702028164,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+               }}                
           />  
         </View>
+        <Title>UNIDADES FISICAS</Title>
+        <TextSubTitle>Delegacia Polícia Civil Ilhéus BA</TextSubTitle>
+        <ContainerText><TitleInContainer>Av. Cel. Misael Tavares, 253 - Centro</TitleInContainer></ContainerText>
+        <TextSubTitle>69ª CIPM - Unidade Policial Militar</TextSubTitle>
+        <ContainerText><TitleInContainer>Caminho 20 - Hernani Sá</TitleInContainer></ContainerText>
+        <TextSubTitle>68ª CIPM - Unidade Policial Militar</TextSubTitle>
+        <ContainerText><TitleInContainer>Rua Dom Bendito Zorzi, nº145, R. dos Carilos - Conquista</TitleInContainer></ContainerText>
+        <TextSubTitle>70ª CIPM - Unidade Policial Militar</TextSubTitle>
+        <ContainerText><TitleInContainer>Km 03, 335, BA-262 - Distrito Industrial</TitleInContainer></ContainerText>
+        <TextSubTitle>Polícia Rodoviária Estadual</TextSubTitle>
+        <ContainerText><TitleInContainer>Rod. Jorge Amado, 112 - Ilhéus, BA</TitleInContainer></ContainerText> 
+        <View style={styles.space}>
+          </View> 
       </ScrollView>
     );
   }
 
   const styles = StyleSheet.create({
     container: {
-      width: 365,
-      height: 56,
+      width: 381,
+      height: 418,
+      marginLeft: 15,
+      marginTop: 15,
+      
+    },
+
+    space:{
+      height: 55,
     },
   })
