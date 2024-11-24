966c3a64501e317083d3753d39a0d57160e573d6
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-11-23 23:16:04 -0300

    Criado campos de contatos e links do site para boletin de ocorrencia

--- src/screens/Contacts/index.tsx
+++ src/screens/Contacts/index.tsx
@@ -1,20 +1,36 @@
-import {ScrollView, Text, View, } from "react-native";
-import { HeaderApp } from "../../components/Header"
-import { Container, SubTitle, TitleSubContainer} from "./style";
-import { useNavigation } from "@react-navigation/native";
-import { RootNavigationProp } from "src/types/navigation";
+  import {ScrollView, Text, View, Linking  } from "react-native";
+  import { HeaderApp } from "../../components/Header"
+  import { Container, SubTitle, TitleSubContainer, Title, TextSubTitle, ContainerText, TitleInContainer, IconContainer, ContainerText2, TitleInContainer2} from "./style";
+  import { useNavigation } from "@react-navigation/native";
+  import { RootNavigationProp } from "src/types/navigation";
+  import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function Contact() {
-  
-  return (
-    <View>
-      <HeaderApp />
-      <SubTitle>
-      <TitleSubContainer>Início</TitleSubContainer>
-      <TitleSubContainer>Formulários</TitleSubContainer>
-      <TitleSubContainer>Mapa</TitleSubContainer>
-      <TitleSubContainer>Contatos</TitleSubContainer>
-      </SubTitle>
-    </View>
-  );
-}
+    export default function Contact() {
+      const openLink = () => {
+        Linking.openURL("https://delegaciavirtual.sinesp.gov.br/portal/");
+      };
+    
+    return (
+      <ScrollView>
+        <HeaderApp />
+        <SubTitle>
+        <TitleSubContainer>Início</TitleSubContainer>
+        <TitleSubContainer>Formulários</TitleSubContainer>
+        <TitleSubContainer>Mapa</TitleSubContainer>
+        <TitleSubContainer>Contatos</TitleSubContainer>
+        </SubTitle>
+        <Title>SITES</Title>
+        <TextSubTitle>Registro de Boletim de Ocorrência</TextSubTitle>
+        <ContainerText><TitleInContainer>https://delegaciavirtual.sinesp.gov.br/portal/</TitleInContainer></ContainerText>
+        <Title>CONTATOS</Title>
+        <TextSubTitle>Emergência Policial</TextSubTitle>
+        <ContainerText><TitleInContainer>190</TitleInContainer></ContainerText>
+        <TextSubTitle>Serviço de atendimento móvel de urgência</TextSubTitle>
+        <ContainerText><TitleInContainer>192</TitleInContainer></ContainerText>
+        <TextSubTitle>Central de atendimento à Mulher</TextSubTitle>
+        <ContainerText><TitleInContainer>180</TitleInContainer></ContainerText>
+        <ContainerText2><IconContainer><Ionicons name="location-sharp" size={40} color="black" /></IconContainer><TitleInContainer2>Mapa - Unidades Policiais</TitleInContainer2></ContainerText2>
+        
+      </ScrollView>
+    );
+  }
--- src/screens/Contacts/style.ts
+++ src/screens/Contacts/style.ts
@@ -29,3 +29,85 @@ export const TitleSubContainer = styled.Text`
     margin-left: 30px;
 
 `;
+
+export const Title = styled.Text`
+
+    font-family: "Roboto-Bold";
+    font-size: 30px;
+    color: #00274D;
+    margin-left: 21px;
+    margin-top: 44px;
+
+`;
+
+export const TextSubTitle = styled.Text`
+
+font-family: "Roboto";
+font-size: 16px;
+color: #00274D;
+margin-left: 21px;
+margin-top: 15px;
+
+`;
+
+export const ContainerText = styled.View`
+
+    width: 388px;
+    height: 69px;
+    padding: 8px;
+    margin-top: 5px;
+    margin-left: 10px;
+    border-radius: 8px;
+    border-color: black;
+    border-width: 0.3px;
+    flex-direction: row;
+    align-items: center;
+    padding-horizontal: 10px;
+`;
+
+export const ContainerText2 = styled.View`
+
+margin-top: 20px;
+  margin-left: 10px;
+  width: 388px;
+  height: 63px;
+  flex-direction: row;
+  background-color: white;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: #ccc;
+  align-items: center;
+  padding-horizontal: 10px;
+`;
+
+export const TitleInContainer = styled.Text`
+
+  font-family: "Roboto";
+  font-size: 22px;
+  color: #00274D;
+  text-align: center;
+  text-decoration-line: underline; /* Indica que é um link */
+  
+
+`;
+
+export const IconContainer = styled.View`
+  
+ 
+  justify-content: center;
+  align-items: center;
+ 
+
+`;
+
+export const TitleInContainer2 = styled.Text`
+
+  font-family: "Roboto";
+  font-size: 22px;
+  color: #00274D;
+  text-align: center;
+  margin-left: 20px;
+  text-decoration-line: underline; /* Indica que é um link */
+  
+
+`;
