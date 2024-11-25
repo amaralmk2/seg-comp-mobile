a69d453d5fdf4c71d141928f18c91f706303f561
Ricardo <ricardo.dev1991@gmail.com>
2024-11-24 21:18:13 -0300

    fix: Ajuste nas funcionalidades do mapa de localizações de unidades policiais e adição de ícones específicos para cada unidade.

--- src/screens/navigation-root.tsx
+++ src/screens/navigation-root.tsx
@@ -5,14 +5,13 @@ import { LoginStack } from '../navigation/LoginStack';
 import { ConctactStack } from '../navigation/ContactStack';
 import { RegisterUserStack } from '../navigation/RegisterUserStack';
 import { HeaderApp } from 'src/components/Header';
-import { ConctactStack } from '../navigation/ContactStack';
 import { OcurrencesMap } from './Map-Ocurrences';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
 export function RootNavigator() {
   return (
-      <RootStack.Navigator initialRouteName="ContactScreen" screenOptions={{ header: () => <HeaderApp />}}>
+      <RootStack.Navigator initialRouteName="LoginScreen" screenOptions={{ header: () => <HeaderApp />}}>
         <RootStack.Screen name="HomeScreen" component={HomeStack} />
         <RootStack.Screen name="LoginScreen" component={LoginStack} />
         <RootStack.Screen name="RegisterUserScreen" component={RegisterUserStack} />
